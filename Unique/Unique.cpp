3fc5c61c5d0d8bb36c1e323bc62e2c0833b0ed94
ValeriyH <ValeriyH@users.noreply.github.com>
2020-06-22 20:47:38 +0300

    Updated Unique. Now it can handle unique string in the list of files base on file pattern.

--- Unique/Unique.cpp
+++ Unique/Unique.cpp
@@ -5,67 +5,89 @@
 #include <stdio.h>
 #include <string>
 #include <map>
+#include <Windows.h>
 
 #define MAX_LINE_SIZE 4096
 
 int main(int argc, char* argv[])
 {
-    if (argc < 2)
+    if (argc < 3)
     {
         puts("This tool check what all lines in the file are unique. All non-unique lines printed.");
         puts("Error level\n\tset to 0 if all lines are unique.\n\tset to 1 if not all arguments is set or fail to open file.\n\tset to 2 if some lines are not unique.");
         printf("Note: line size is limited to %d chars\n\n", MAX_LINE_SIZE);
-        printf("Usage %s <filename>\n", argv[0]);
+        printf("Note: folder should end with \\ character");
+        printf("Usage %s <folder> <filename_pattern>\n", argv[0]);
         return 1;
     }
-    else
+
+    std::string dir = argv[1];
+    std::string file_pattern = argv[2];
+    WIN32_FIND_DATA data;
+    HANDLE hFind;
+    if ((hFind = FindFirstFile((dir + file_pattern).c_str(), &data)) == INVALID_HANDLE_VALUE)
     {
-        printf("Reading file %s\n", argv[1]);
+        perror("Can't open folder");
+        return 1;
     }
 
     int ret = 0;
-    FILE * pFile = NULL;
-    char mystring[MAX_LINE_SIZE];
-
-    fopen_s(&pFile, argv[1], "r");
-    if (pFile == NULL)
+    std::map<std::string, int> matched;
+    do 
     {
-        perror("Error opening file");
-        return 1;
-    }
-    else
+        if (data.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)
+        {
+            continue;
+        }
+
+        printf("Reading file %s\n", data.cFileName);
+        FILE* pFile = NULL;
+        char mystring[MAX_LINE_SIZE];
+
+        fopen_s(&pFile, (dir + data.cFileName).c_str(), "r");
+        if (pFile == NULL)
+        {
+            perror("Error opening file:");
+            perror(data.cFileName);
+            ret = ret == 0 ? 1 : ret;
+            continue;
+        }
+        else
+        {
+             while (fgets(mystring, MAX_LINE_SIZE, pFile) != NULL)
+            {
+                int len = strlen(mystring);
+                if (mystring[len - 2] == '\r')
+                {
+                    mystring[len - 2] = 0;
+                }
+                else if (mystring[len - 1] == '\n')
+                {
+                    mystring[len - 1] = 0;
+                }
+
+                matched[mystring]++;
+            }
+            fclose(pFile);
+        }
+    } 
+    while (FindNextFile(hFind, &data) != 0);
+    FindClose(hFind);
+
+    for each (auto line in matched)
     {
-        std::map<std::string, int> matched;
-        while (fgets(mystring, MAX_LINE_SIZE, pFile) != NULL)
+        if (line.second > 1)
         {
-            int len = strlen(mystring);
-            if (mystring[len - 2] == '\r')
-            {
-                mystring[len - 2] = 0;
-            }
-            else if (mystring[len - 1] == '\n')
-            {
-                mystring[len - 1] = 0;
-            }
-
-            matched[mystring]++;
-        }
-        fclose(pFile);
-
-        for each (auto line in matched)
-        {
-            if (line.second > 1)
-            {
-                printf("Line '%s' occurs %d times\n", line.first.c_str(), line.second);
-                ret = 2;
-            }
-        }
-
-        if (ret == 0)
-        {
-            puts("All lines are uniuqe.");
+            printf("Line '%s' occurs %d times\n", line.first.c_str(), line.second);
+            ret = 2;
         }
     }
+
+    if (ret == 0)
+    {
+        puts("All lines are uniuqe.");
+    }
+
 
     return ret;
 }
